07beec0e491fb1c5eec91eed1f120030c84ce220
Ivan Kleynhans <info@linivu.com>
2021-08-10 17:05:25 +0200

    Add Logo component

---
+++ src/components/global/Logo.jsx
@@ -0,0 +1,12 @@
+// STYLES //
+import './Logo.scss';
+
+const Logo = () => {
+  return (
+    <a href="/" className="logo">
+      DISCO<span>V</span>R<span>.</span>
+    </a>
+  );
+};
+
+export default Logo;
