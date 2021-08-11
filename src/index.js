9db16d443a7695267f461a11e2e114e693e72c99
Ivan Kleynhans <info@linivu.com>
2021-08-11 09:27:54 +0200

    Update index.js: add transitionPause

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { render as Render } from 'react-dom';
 
+import transitionPause from './utilities/transitionPause';
+
 // COMPONENTS //
 import App from './App';
 
 Render(<App />, document.getElementById('app'));
+
+// TRANSITION PAUSE //
+transitionPause();
